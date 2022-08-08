5472ba5dde4e6fc62e80abb370ea00153cbfd2ab
Kohei Terakawa <odayakalife@teraka.local>
2022-08-08 09:38:49 +0900

    SVG画像のclickイベントを検知するように

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import { Button, ButtonGroup } from '@chakra-ui/react'
 export default function Home() {
   return (
     <div className={styles.container}>
