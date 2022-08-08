a58c715da28efc6bbdd0864ff1dd0f6477be5c42
10matcho27 <ryo.1027.smile@gmail.com>
2022-08-08 15:54:53 +0900

    辞書型json読み込めるようになった

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,8 +12,9 @@ import {
 } from '@chakra-ui/react'
 
 import { ChakraProvider } from '@chakra-ui/react'
-import { useState } from "react";
-export default function Home() {
+import { useState } from "react"
+
+export default function Home(data) {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [size, setSize] = useState('md')
   const [spot, setSpot] = useState('')
@@ -24,17 +25,29 @@ export default function Home() {
   }
 
   const openModal = (spot) => {
-
-    console.log(spot);
+    console.log(spot)
     setSize('full')
     setSpot(spot)
     onOpen()
   }
 
   const sizes = ['xs', 'sm', 'md', 'lg', 'xl', 'full']
+  console.log(data)
   return (
     <div>
-      <SVGMap onClickSpot={openModal} />
+
+      {/* {data.posts.map((item) => {
+        return (
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+            <button onClick={onOpen}>Open Modal</button>
+          </div>
+        )
+      })
+      } */}
+
+      <SVGMap aaaaaaaaaaaaaaa={openModal} />
       {sizes.map((size) => (
         <Button
           onClick={() => handleSizeClick(size)}
@@ -55,8 +68,15 @@ export default function Home() {
           </ModalFooter>
         </ModalContent>
       </Modal>
-
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  // const res = await fetch('https://jsonplaceholder.typicode.com/users') // api call
+  const res = await fetch('http://localhost:5000/event') // api call
+  const spot = await res.json()
+  console.log("fdsafdsafdsa")
+  return { props: { spot } }
 }
 
