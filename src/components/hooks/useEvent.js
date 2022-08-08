78ea137c9aee92e5fce3fab3383ef27afa0d51e7
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-08 18:02:21 +0900

    implement datePicker

--- src/components/hooks/useEvent.js
+++ src/components/hooks/useEvent.js
@@ -4,6 +4,7 @@ export const useEvent = () => {
   const [event, setEvent] = useState('');
   const [description, setDescription] = useState('');
   const [place, setPlace] = useState('');
+  const [date, setDate] = useState(new Date());
 
   const send = async () => {
     await fetch('https://httpbin.org/post', {
@@ -11,11 +12,12 @@ export const useEvent = () => {
       headers: {
         'Content-Type': 'application/json'
     },
-      body: JSON.stringify({event:event, place:place, description: description}),
+    // dateはUTCに変換される
+      body: JSON.stringify({event:event, date:date, place:place, description: description}),
     }).then(response => console.log(response));
   };
 
   return {
-    setEvent, setDescription, setPlace, send,
+    setEvent, setDescription, setPlace, date, setDate, send,
   };
 };
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -4,26 +4,45 @@ import {
     FormLabel,
     Textarea,
     Input,
-    Select
-  } from '@chakra-ui/react'
+    Select,
+} from '@chakra-ui/react'
+import DatePicker, { registerLocale } from "react-datepicker";
+import ja from "date-fns/locale/ja";
+import "react-datepicker/dist/react-datepicker.css"
 
 import { useEvent } from '../components/hooks/useEvent';
 
-export default function Admin() {
-const { setEvent, setDescription, setPlace, send } = useEvent();
 
-return (
-    <Box margin={"5px"}>
-        <FormLabel>イベント名</FormLabel>
-        <Input type="text" onChange={(e) => setEvent(e.target.value)} />
-        <FormLabel>開催場所</FormLabel>
-        <Select placeholder='開催場所を選択してください' onChange={(e) => setPlace(e.target.value)} >
-            <option value='option1'>中央公園前</option>
-            <option value='option2'>えるむプラザ</option>
-        </Select>
-        <FormLabel>イベントの詳細</FormLabel>
-        <Textarea onChange={(e) => setDescription(e.target.value)} />
-        <Button type="button" onClick={send}>作成する</Button>
-    </Box>
-);
+export default function Admin() {
+    const { setEvent, setDescription, setPlace, date, setDate,  send } = useEvent();
+    const Today = new Date();
+    registerLocale('ja', ja);
+
+    return (
+        <Box margin={"5px"}>
+            <FormLabel>イベント名</FormLabel>
+            <Input type="text" onChange={(e) => setEvent(e.target.value)} />
+            <FormLabel>開催日時</FormLabel>
+            <field>
+                <DatePicker 
+                    dateFormat="yyyy/MM/dd HH:mm"
+                    selected={date}
+                    locale='ja'
+                    showTimeSelect
+                    onChange={selectedDate => {
+                        console.log(selectedDate);
+                        setDate(selectedDate)}}
+                />
+            </field>
+            
+            <FormLabel>開催場所</FormLabel>
+            <Select placeholder='開催場所を選択してください' onChange={(e) => setPlace(e.target.value)} >
+                <option value='option1'>中央公園前</option>
+                <option value='option2'>えるむプラザ</option>
+            </Select>
+            <FormLabel>イベントの詳細</FormLabel>
+            <Textarea onChange={(e) => setDescription(e.target.value)} />
+            <Button type="button" onClick={send}>作成する</Button>
+        </Box>
+    );
 }
