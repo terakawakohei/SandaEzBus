774cf71e7f3fedc053bb38766bb9419942522dcb
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-11 00:58:38 +0900

    add NEXTAUTH_SECRET

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
   env: {
     // NEXTAUTH_URL: "https://deploy-preview-53--spiffy-dolphin-ef62d9.netlify.app",
     USER_EMAIL:"admin",
-    USER_PASSWORD:"1234"
+    USER_PASSWORD:"1234",
+    NEXTAUTH_SECRET:"2aTMoyJqkjReB5Y7JgX8VGk+GSHaeEClSSF1nrGfktE="
   },
 }
 
