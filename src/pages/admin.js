8a0239507c5c74345fe4ad15ee04e2bb2d76a4ab
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-10 21:14:09 +0900

    implement auth for admin page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,9 @@
-import { Link, Button, Flex } from '@chakra-ui/react'
+import { useSession, signIn, signOut } from "next-auth/react"
+
 import {
     Box,
+    Button,
+    Flex,
     FormLabel,
     Textarea,
     Input,
@@ -14,35 +17,44 @@ import { useEvent } from '../components/hooks/useEvent';
 
 
 export default function Admin() {
+    const { data: session } = useSession()
     const { setEvent, setDescription, setPlace, date, setDate,  send } = useEvent();
     const Today = new Date();
     registerLocale('ja', ja);
+    if (session) {
+        return (
+            <Box margin={5}>
+                <FormLabel>イベント名</FormLabel>
+                <Input type="text" onChange={(e) => setEvent(e.target.value)} />
+                <FormLabel>開催日時</FormLabel>
+                <field>
+                    <DatePicker 
+                        dateFormat="yyyy/MM/dd HH:mm"
+                        selected={date}
+                        locale='ja'
+                        showTimeSelect
+                        onChange={selectedDate => {
+                            console.log(selectedDate);
+                            setDate(selectedDate)}}
+                    />
+                </field>
+                
+                <FormLabel>開催場所</FormLabel>
+                <Select placeholder='開催場所を選択してください' onChange={(e) => setPlace(e.target.value)} >
+                    <option value='option1'>中央公園前</option>
+                    <option value='option2'>えるむプラザ</option>
+                </Select>
+                <FormLabel>イベントの詳細</FormLabel>
+                <Textarea onChange={(e) => setDescription(e.target.value)} />
+                <Button type="button" onClick={send}>作成する</Button>
+            </Box>
+        );
+    }
 
-    return (
-        <Box margin={5}>
-            <FormLabel>イベント名</FormLabel>
-            <Input type="text" onChange={(e) => setEvent(e.target.value)} />
-            <FormLabel>開催日時</FormLabel>
-            <field>
-                <DatePicker 
-                    dateFormat="yyyy/MM/dd HH:mm"
-                    selected={date}
-                    locale='ja'
-                    showTimeSelect
-                    onChange={selectedDate => {
-                        console.log(selectedDate);
-                        setDate(selectedDate)}}
-                />
-            </field>
-            
-            <FormLabel>開催場所</FormLabel>
-            <Select placeholder='開催場所を選択してください' onChange={(e) => setPlace(e.target.value)} >
-                <option value='option1'>中央公園前</option>
-                <option value='option2'>えるむプラザ</option>
-            </Select>
-            <FormLabel>イベントの詳細</FormLabel>
-            <Textarea onChange={(e) => setDescription(e.target.value)} />
-            <Button type="button" onClick={send}>作成する</Button>
-        </Box>
-    );
+  return (
+    <Flex h='80vh' justify='center' align='center' direction='column'>
+        <Box>Not signed in</Box>
+        <Box><Button onClick={() => signIn()}>Sign in</Button></Box>
+    </Flex>
+    )
 }
