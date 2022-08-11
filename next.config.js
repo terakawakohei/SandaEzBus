3de5b07598cc23793d0241b07b7bc927d605a0d9
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-11 11:58:04 +0900

    delete NEXTAUTH_URL

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    NEXTAUTH_URL: "http://localhost:3000",
+    // NEXTAUTH_URL: "http://localhost:3000",
     USER_ID:"admin",
     USER_PASSWORD:"1234",
     NEXTAUTH_SECRET:"2aTMoyJqkjReB5Y7JgX8VGk+GSHaeEClSSF1nrGfktE="
