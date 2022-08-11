04549ec7a0a87ad7c93df4a71d3a9f38ba431586
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-08-11 11:55:45 +0900

    update

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    // NEXTAUTH_URL: "https://deploy-preview-53--spiffy-dolphin-ef62d9.netlify.app",
-    USER_EMAIL:"admin",
+    NEXTAUTH_URL: "http://localhost:3000",
+    USER_ID:"admin",
     USER_PASSWORD:"1234",
     NEXTAUTH_SECRET:"2aTMoyJqkjReB5Y7JgX8VGk+GSHaeEClSSF1nrGfktE="
   },
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,47 +1,27 @@
 import NextAuth from 'next-auth'
 import CredentialsProvider from "next-auth/providers/credentials";
 
-// credentials の情報から、ログイン可能か判定してユーザー情報を返す関数
-const findUserByCredentials = credentials => {
-  // 今回は簡易的な例なのでメールアドレスとパスワードが一致する場合にユーザー情報を返却する。
-  // データベースでユーザーを管理している場合は、データベースからユーザーを取得して、パスワードハッシュを比較して判定するのがよいかと。
-  if (
-    credentials.email === process.env.USER_EMAIL &&
-    credentials.password === process.env.USER_PASSWORD
-  ) {
-    // ログイン可ならユーザー情報を返却
-    return true; 
-  } else {
-    // ログイン不可の場合は null を返却
-    return null
-  }
-}
-
-// NextAuth に渡すオプション
-const options = {
-  // 認証プロバイダー
-  providers: [
-    CredentialsProvider({
-      // 表示名 ('Sign in with ...' に表示される)
-      name: "Email",
-      credentials: {
-        email: { label: "User", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      // 認証の関数
-      authorize: async credentials => {
-        const user = findUserByCredentials(credentials)
-        if (user) {
-	  // 返されたオブジェクトはすべてJWTの`user`プロパティに保存される
-          return Promise.resolve(user)
-        } else {
-	  // nullまたはfalseを返すと、認証を拒否する
-          return Promise.resolve(null)
-	  
-        }
-      },
-    }),
-  ],
-}
-
-export default (req, res) => NextAuth(req, res, options)
+export default NextAuth({
+    providers: [
+      CredentialsProvider({
+        name: "Credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          async authorize(credentials) {
+            if (
+                credentials.username === process.env.USER_ID &&
+                credentials.password === process.env.USER_PASSWORD
+              ) {
+                return true
+              } else {
+                return null
+              }
+          },
+      }),
+    ],
+  })
