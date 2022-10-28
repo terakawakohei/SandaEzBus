a3c449643ba79559f442e502f9dcdb46d8ee2783
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-10-28 13:58:08 +0900

    initialize next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXTAUTH_URL:"https://deploy-preview-53--sanda-ez-bus.netlify.app"
-  },
 }
 
 module.exports = nextConfig
