e1e2b6805d9ca463147a13376ce7e9a1d5bf5a35
10matcho27 <ryo.1027.smile@gmail.com>
2022-10-29 00:17:24 +0900

    envの項目削除

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
-    NEXTAUTH_URL:"https://deploy-preview-87--sanda-ez-bus.netlify.app"
-  },
-  //↑絶対消せ！！！
 }
 
 module.exports = nextConfig
