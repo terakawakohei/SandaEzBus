7e3b356d49cf4736be03fc8ee0c362eb8b1842e2
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-10-28 13:33:19 +0900

    nextconfig

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
     // NEXTAUTH_URL: "http://localhost:3000",
     USER_ID:"admin",
     USER_PASSWORD:"1234",
-    NEXTAUTH_SECRET:"2aTMoyJqkjReB5Y7JgX8VGk+GSHaeEClSSF1nrGfktE="
+    NEXTAUTH_SECRET:"2aTMoyJqkjReB5Y7JgX8VGk+GSHaeEClSSF1nrGfktE=",
+    NEXTAUTH_URL:"https://deploy-preview-53--spiffy-dolphin-ef62d9.netlify.app"
   },
 }
 
