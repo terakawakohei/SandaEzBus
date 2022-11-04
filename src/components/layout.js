2c22074a39540516325bc516e6f8db25d16ee07b
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-11-04 11:05:48 +0900

    add meta discription

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,7 @@ export default function Layout({ children }) {
         <>
             <Header />
             <title>Sanda Ez Bus</title>
+            <meta name="description" content="兵庫県三田市内のイベント告知・経路案内を行います。" />
             <main>{children}</main>
             {/* <Footer /> */}
         </>
